5878b3dcac2e6977dc0e8434fbf67915981b037b
Gabriel <gabriel.mendes@dataway.info>
2023-04-25 17:21:05 -0400

    commit 25-04

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 
 const App = () =>{
-  return(
-    <View>
-
-    </View>
-  )
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name=""  />
+    </Stack.Navigator>
+  );
 }
 
 export default App
---
+++ screens/login.js
@@ -0,0 +1,9 @@
+import React from "react-native";
+
+
+
+const Login = (props) => {
+
+}
+
+export default Login;
